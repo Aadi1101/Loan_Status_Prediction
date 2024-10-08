69b4954a173fcc48e78fa39def55c6dfaed9fc82
Aaditya Komerwar <aadityakomerwar@gmail.com>
2024-10-08 23:35:29 +0530

    Model Training with Hyperparameter tuning and API Creation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -21,7 +21,7 @@ class DataIngestion():
 
     def initiate_data_ingestion(self):
         try:
-            df = pd.read_csv('notebooks\loan.csv')
+            df = pd.read_csv(r'notebooks\loan.csv')
             logging.info("Read the data")
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
